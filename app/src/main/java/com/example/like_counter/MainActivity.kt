712c7cf2a9ea59ef6d27524d14c9f9862f5c99c6
pasan wedikkara <pasan.madusanka9@gmail.com>
2023-02-12 18:49:31 +0900

    data binding added

--- app/src/main/java/com/example/like_counter/MainActivity.kt
+++ app/src/main/java/com/example/like_counter/MainActivity.kt
@@ -1,24 +1,27 @@
 package com.example.like_counter
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.example.like_counter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     val viewModel:LikeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val  binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        val likeButton:Button=findViewById(R.id.buttonLike)
-        val likecounttext:TextView=findViewById(R.id.textViewlikecount)
-        likeButton.setOnClickListener {
+            // val likeButton:Button=findViewById(R.id.buttonLike)
+        //val likecounttext:TextView=findViewById(R.id.textViewlikecount)
+        binding.buttonLike.setOnClickListener {
 
 viewModel.perform_Like_count()
-            likecounttext.text=viewModel.likecount.toString()
+            binding.textViewlikecount.text=viewModel.likecount.toString()
 
         }
     }
