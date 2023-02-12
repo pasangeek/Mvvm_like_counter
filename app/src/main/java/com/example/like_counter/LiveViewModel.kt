4835000be16dbee7f872379750129053b3edce07
pasan wedikkara <pasan.madusanka9@gmail.com>
2023-02-12 18:17:08 +0900

    ui added

---
+++ app/src/main/java/com/example/like_counter/LiveViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.like_counter
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LiveViewModel{
+
+    var livecount:Int = 0
+    fun perform
+
+
+}
