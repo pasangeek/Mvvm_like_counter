0920ac81f3701c067c8a6dcdc033a5b44169cc72
pasan wedikkara <pasan.madusanka9@gmail.com>
2023-02-12 21:39:23 +0900

    observer added

--- app/src/main/java/com/example/like_counter/LikeViewModel.kt
+++ app/src/main/java/com/example/like_counter/LikeViewModel.kt
@@ -1,14 +1,25 @@
 package com.example.like_counter
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 
 class LikeViewModel:ViewModel(){
+    private val _likecount = MutableLiveData <Int>(0)
+    private val _dislikecount = MutableLiveData <Int>(0)
 
-    var likecount:Int = 0
-    fun perform_Like_count(){
+    val likecount  : LiveData<Int>
+       get() = _likecount
+    val dislikecount : LiveData<Int>
+        get() = _dislikecount
+    //var likecount:Int = 0
+    fun performLike(){
+        _likecount.value  =_likecount.value!!+1
+
+//likecount++
 
 
-likecount++
 
         }
 
--- app/src/main/java/com/example/like_counter/MainActivity.kt
+++ app/src/main/java/com/example/like_counter/MainActivity.kt
@@ -1,28 +1,33 @@
 package com.example.like_counter
 
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.example.like_counter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    val viewModel:LikeViewModel by viewModels()
+    val viewModel: LikeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val  binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-            // val likeButton:Button=findViewById(R.id.buttonLike)
-        //val likecounttext:TextView=findViewById(R.id.textViewlikecount)
+        binding.likeVM = viewModel
         binding.buttonLike.setOnClickListener {
 
-viewModel.perform_Like_count()
-            binding.textViewlikecount.text=viewModel.likecount.toString()
+            viewModel.performLike()
+            //  binding.textViewlikecount.text = viewModel.likecount.toString()
 
         }
-    }
+
+        //----observer -----
+        val likeObserver = Observer<Int> {
+                newV -> binding.textViewlikecount.text = newV.toString()
+        }
+        viewModel.likecount.observe(this, likeObserver)
+
+}
 }
