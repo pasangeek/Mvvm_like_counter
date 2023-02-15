83d5d62226569865a5fa953acfbdfeeac9e1d0ac
pasan wedikkara <pasan.madusanka9@gmail.com>
2023-02-15 23:20:21 +0900

    shared preferences added

--- app/src/main/java/com/example/like_counter/MainActivity.kt
+++ app/src/main/java/com/example/like_counter/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.like_counter
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -9,11 +12,17 @@ import com.example.like_counter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     val viewModel: LikeViewModel by viewModels()
-
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding =
+
+
+binding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+
+    //Data binding
 
         binding.likeVM = viewModel
         binding.lifecycleOwner  = this
@@ -22,19 +31,51 @@ class MainActivity : AppCompatActivity() {
             viewModel.performLike()
             //  binding.textViewlikecount.text = viewModel.likecount.toString()
 
+
+
         }
-        binding.likeVM = viewModel
-        binding.lifecycleOwner  = this
+
         binding.buttondislike.setOnClickListener {
 
             viewModel.performDisLike()
         }
+    }
+        override fun onPause() {
+            super.onPause()
 
-        //----observer -----
-       /* val likeObserver = Observer<Int> {
-                newV -> binding.textViewlikecount.text = newV.toString()
+            val sharedPref = getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
+            val editor = sharedPref.edit()
+
+            editor.apply {
+
+                putString("LIKE", viewModel.likecount.value.toString())
+              putString("DISLIKE", viewModel.dislikecount.value.toString())
+                apply()
+            }
+
         }
-        viewModel.likecount.observe(this, likeObserver)*/
+
+        override fun onResume() {
+            super.onResume()
+            val sharedPref = getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
+
+            val load = sharedPref.getString("LIKE", null)
+           val load1 = sharedPref.getString("DISLIKE", null)
+
+            binding.textViewlikecount.setText(load)
+          binding.textViewDislikecount.setText(load1)
+
+
+
+        } //----observer -----
+    /* val likeObserver = Observer<Int> {
+             newV -> binding.textViewlikecount.text = newV.toString()
+     }
+     viewModel.likecount.observe(this, likeObserver)*/
 
 }
-}
+
+
+
+
+
